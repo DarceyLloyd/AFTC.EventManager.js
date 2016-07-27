f40f318d1382ed9a9fa7640ba9a54677927e4b35
Darcey <Darcey@AllForTheCode.co.uk>
2016-07-27 03:41:27 +0100

    improvements
    various

--- EventManager.js
+++ EventManager.js
@@ -6,19 +6,25 @@
  */
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function log(arg) {
-	console.log(arg);
+if (!log){
+	function log(arg){
+		if (console){
+			console.log(arg);
+		}
+	}
 }
-function getFunctionName(callback) {
-	var name = callback.toString();
-	var reg = /function ([^\(]*)/;
-	return reg.exec(name)[1];
+if (!Function){
+	function getFunctionName(callback){
+		var name = callback.toString();
+		var reg = /function ([^\(]*)/;
+		return reg.exec(name)[1];
+	}
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function EventManagerEventVO() {
+function EventManagerEventVO(){
 	this.name = "";
 	this.dispatchFunctions = [];
 }
@@ -26,17 +32,16 @@ function EventManagerEventVO() {
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-var EventManager = (function () {
+var EventManager = (function(){
 	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	var events = [];
 	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-
 	
 	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	return {
 		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		add: function (eventName, dispatchFunction) {
+		add:function(eventName, dispatchFunction){
 			// log("######################################");
 			// log("add("+eventName+","+getFunctionName(dispatchFunction)+")");
 			// log("### BEFORE: [" + events.length + "]");
@@ -48,15 +53,15 @@ var EventManager = (function () {
 			// Check event exists
 			var eventExists = false;
 			var foundOnIndex = -1;
-			for (i = 0; i < events.length; i++) {
-				if (events[i].name === eventName) {
+			for (var i = 0; i < events.length; i++) {
+				if (events[i].name === eventName){
 					foundOnIndex = i;
 					eventExists = true;
 					break;
 				}
 			}
 			
-			if (!eventExists) {
+			if (!eventExists){
 				var vo = new EventManagerEventVO();
 				vo.name = eventName;
 				vo.dispatchFunctions.push(dispatchFunction);
@@ -65,15 +70,15 @@ var EventManager = (function () {
 				// Don't add duplicate functions
 				var functionAlreadyAdded = false;
 				for (i = 0; i < events[foundOnIndex].dispatchFunctions.length; i++) {
-					if (events[foundOnIndex].dispatchFunctions[i] == dispatchFunction) {
+					if (events[foundOnIndex].dispatchFunctions[i] == dispatchFunction){
 						functionAlreadyAdded = true;
 						break;
 					}
 				}
-				if (!functionAlreadyAdded) {
+				if (!functionAlreadyAdded){
 					events[foundOnIndex].dispatchFunctions.push(dispatchFunction);
 				} else {
-					log("EventManager.add: Event [" + eventName + "] already dispatches a function named [" + getFunctionName(dispatchFunction)+ "]");
+					log("EventManager.add: Event [" + eventName + "] already dispatches a function named [" + getFunctionName(dispatchFunction) + "]");
 				}
 			}
 			
@@ -85,8 +90,8 @@ var EventManager = (function () {
 			// log("######################################");
 		},
 		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		remove: function (eventName, dispatchFunction) {
-			if (dispatchFunction == "" || dispatchFunction == null || !dispatchFunction) {
+		remove:function(eventName, dispatchFunction){
+			if (dispatchFunction == "" || dispatchFunction == null || !dispatchFunction){
 				log("EventManager.remove: Incorrect use of remove function, check args.");
 				return;
 			}
@@ -99,35 +104,39 @@ var EventManager = (function () {
 			// 	log(events[i]);
 			// }
 			
+			var vo;
+			
 			// Check event exists
 			var eventExists = false;
 			var foundOnIndex = -1;
 			for (var i = 0; i < events.length; i++) {
-				if (events[i].name === eventName) {
+				if (events[i].name === eventName){
 					foundOnIndex = i;
 					eventExists = true;
 					break;
 				}
 			}
 			
-			if (eventExists) {
-				var vo = events[foundOnIndex];
+			if (eventExists){
+				vo = events[foundOnIndex];
 				for (i = 0; i < vo.dispatchFunctions.length; i++) {
-					if (vo.dispatchFunctions[i] === dispatchFunction) {
+					if (vo.dispatchFunctions[i] === dispatchFunction){
 						vo.dispatchFunctions[i] = null;
 						vo.dispatchFunctions.splice(i, 1);
 						break;
 					}
 				}
-				
 			} else {
 				log("EventManager.remove: Cannot remove dispatch function from event [" + eventName + "] which doesn't exist!");
 			}
 			
 			// If no functions for dispatch on event name remaining remove the event
-			if (vo.dispatchFunctions.length < 1){
-				this.removeAll(eventName);
+			if (vo.dispatchFunctions != undefined){
+				if (vo.dispatchFunctions.length < 1){
+					this.removeAll(eventName);
+				}
 			}
+			
 			
 			// log("### AFTER: [" + events.length + "]");
 			// log(events);
@@ -137,11 +146,11 @@ var EventManager = (function () {
 			// log("######################################");
 		},
 		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		dispatch: function (eventName) {
+		dispatch:function(eventName){
 			// log("dispatch("+eventName+")");
 			var eventExists = false;
 			for (var i = 0; i < events.length; i++) {
-				if (events[i].name === eventName) {
+				if (events[i].name === eventName){
 					eventExists = true;
 					// Call all event dispatch functions
 					var eventDispatchFunctions = events[i].dispatchFunctions;
@@ -151,13 +160,13 @@ var EventManager = (function () {
 					break;
 				}
 			}
-			if (!eventExists) {
+			if (!eventExists){
 				log("EventManager.dispatch: Event [" + eventName + "] doesn't exist!");
 				return false;
 			}
 		},
 		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-		removeAll: function (eventName) {
+		removeAll:function(eventName){
 			// log("######################################");
 			// log("removeAll("+eventName+")");
 			// log("### BEFORE: [" + events.length + "]");
@@ -166,12 +175,12 @@ var EventManager = (function () {
 			// 	log(events[i]);
 			// }
 			
-			if (!eventName || eventName == "" || eventName == null) {
+			if (!eventName || eventName == "" || eventName == null){
 				events = null;
 				events = [];
 			} else {
 				for (var i = 0; i < events.length; i++) {
-					if (events[i].name === eventName) {
+					if (events[i].name === eventName){
 						events[i].name = null;
 						events[i].dispatchFunctions = null;
 						events[i] = null;
@@ -226,11 +235,11 @@ var EventManager = (function () {
 
 // Usage example 4: Cleaning everything up
 /*
-function myEventDispatchHandler1(){ console.log("myEventDispatchHandler1(): Executed"); }
-function myEventDispatchHandler2(){ console.log("myEventDispatchHandler2(): Executed"); }
-EventManager.add("MyEvent1",myEventDispatchHandler1);
-EventManager.add("MyEvent2",myEventDispatchHandler2);
-EventManager.removeAll();
-EventManager.dispatch("MyEvent1");
-EventManager.dispatch("MyEvent2");
-*/
+ function myEventDispatchHandler1(){ console.log("myEventDispatchHandler1(): Executed"); }
+ function myEventDispatchHandler2(){ console.log("myEventDispatchHandler2(): Executed"); }
+ EventManager.add("MyEvent1",myEventDispatchHandler1);
+ EventManager.add("MyEvent2",myEventDispatchHandler2);
+ EventManager.removeAll();
+ EventManager.dispatch("MyEvent1");
+ EventManager.dispatch("MyEvent2");
+ */
